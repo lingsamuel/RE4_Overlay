3cfb61a875af8f93ec2bcda660eb0b681c3172fc
Pegaiur <pegaiur@gmail.com>
2023-03-25 17:36:46 +0800

    config part

--- RE4_Overlay.lua
+++ RE4_Overlay.lua
@@ -62,11 +62,74 @@ local function GetPlayerManager()
 	return PlayerManager
 end
 
+-- ==== Config ====
+
+local LOAD_CONFIG_FILE = true
+
+local Config = {}
+Config.uiConfig = {}
+Config.dingConfig = {}
+
+local configPath = "RE4_Overlay/RE4_Overlay.json"
+
+function Config.Init()
+    local defaultUI = {}
+    defaultUI.Font = nil
+    defaultUI.Row = 0
+    defaultUI.RowHeight = 25
+    defaultUI.PosX = 1400
+    defaultUI.PosY = 200
+
+    local defaultDing = {}
+    defaultDing.invincible = false
+
+    Config.uiConfig = defaultUI
+    Config.dingConfig = defaultDing
+end
+
+function Config.LoadConfig()
+	local configFile = json.load_file(configPath)
+	if configFile.dingConfig ~= nil then
+		Config.uiConfig = configFile.uiConfig
+        Config.dingConfig = configFile.dingConfig
+	else
+        local newConfig = {}
+        newConfig.uiConfig = Config.uiConfig
+        newConfig.dingConfig = Config.dingConfig
+		json.dump_file(configPath, newConfig)
+	end
+end
+
+function Config.SaveConfig()
+    local newConfig = {}
+    newConfig.uiConfig = Config.uiConfig
+    newConfig.dingConfig = Config.dingConfig
+	if json.load_file(configPath) ~= newConfig then
+		json.dump_file(configPath, newConfig)
+	end
+end
+
+function Config.SwitchDing(dingStr)
+	if Config.dingConfig[dingStr] == nil then return end
+    Config.dingConfig[dingStr] = not Config.dingConfig[dingStr]
+end
+
+function Config.BoolStateStr(stateStr)
+	if Config.dingConfig[stateStr] == nil then return "error" end
+    if Config.dingConfig[stateStr] then return "enabled" end
+    return "disabled"
+end
+
+Config.Init()
+if LOAD_CONFIG_FILE then
+	Config.LoadConfig()
+end
+
 -- ==== Utils ====
 
 local function SetInvincible(playerBaseContext)
     if playerBaseContext == nil then return end
-
+    if Config.dingConfig.invincible == false then return end
     local hp = playerBaseContext:call("get_HitPoint")
     -- shouldn't call Set_XXX in real life, it may break the save
     -- hp:call("set_Invincible", true)
@@ -109,6 +172,26 @@ local function FloatColumn(val)
 end
 
 -- ==== Draw UI ====
+
+re.on_draw_ui(function()
+    if imgui.tree_node("RE4_Overlay") then
+        if imgui.tree_node("Cheat Utils") then
+            if imgui.button("Invincible") then
+			    Config.SwitchDing("invincible")
+		    end
+            imgui.text(Config.BoolStateStr("invincible"))
+            imgui.tree_pop()
+        end
+        if imgui.button("Save Config") then
+			Config.SaveConfig()
+		end
+        if imgui.button("Reload Config") then
+			Config.LoadConfig()
+		end
+        imgui.text("RE4_Overlay")
+        imgui.tree_pop()
+    end
+end)
 
 local font
 local function initFont()
